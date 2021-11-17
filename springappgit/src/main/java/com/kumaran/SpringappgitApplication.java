ad2a996126a5c30974cd516467df686d8eeb6bc5
punga <1812038@nec.edu.in>
2021-11-17 21:49:07 +0530

    first update

---
+++ springappgit/src/main/java/com/kumaran/SpringappgitApplication.java
@@ -0,0 +1,14 @@
+package com.kumaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringappgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappgitApplication.class, args);
+	//abilazh
+	}
+
+}
---
+++ springappgit/src/test/java/com/kumaran/SpringappgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
